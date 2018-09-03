72617eea8fe883946b71a0e2ad01a13a4a79fe14
Prashant Singh Pawar <prashantpawar@gmail.com>
2018-09-03 15:13:38 -0400

    - more customization to the keyboard firmware

--- orange_planck/orange/keymap.c
+++ orange_planck/orange/keymap.c
@@ -56,14 +56,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * |(Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |)Rshft|
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | LCtrl| LGui | Alt  |   ,  |Lower |    Space    |Raise |   L1 | Down |  Up  |Right |
+ * | LCtrl| LGui | Alt  |   ,  |Lower |    Space    |Raise |  LT1 |  LT2 |  Up  |Right |
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = {
   {GRAVE_ESC,     KC_Q,    KC_W,    KC_E,          KC_R,       KC_T,     KC_Y,   KC_U,       KC_I,         KC_O,         KC_P,         KC_BSPC},
   {CTL_T(KC_TAB), KC_A,    KC_S,    KC_D,          KC_F,       KC_G,     KC_H,   KC_J,       KC_K,         KC_L,         KC_SCLN,      KC_ENT},
   {KC_LSPO,       KC_Z,    KC_X,    KC_C,          KC_V,       KC_B,     KC_N,   KC_M,       KC_COMM,      KC_DOT,       KC_SLSH,      KC_RSPC },
-  {KC_LCTL,       KC_LGUI, KC_LALT, MO(_LAYER_1),  MO(_LOWER), SPCBAR,   SPCBAR, MO(_RAISE), MO(_LAYER_1), MO(_LAYER_2), MO(_LAYER_3), MO(_LAYER_4)}
+  {KC_LCTL,       KC_LGUI, KC_LALT, MO(_LAYER_1),  MO(_LOWER), SPCBAR,   SPCBAR, MO(_RAISE), LT(_LAYER_1, KC_F15), LT(_LAYER_2, KC_F16), LT(_LAYER_3, KC_F17), LT(_LAYER_4, KC_F18)}
 },
 
 /* Lower
@@ -72,16 +72,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * | Del  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   _  |   +  |   {  |   }  |      |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |   '  |ISO | | Home | End  |  |   |
+ * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |      |   '  |      |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |             |      | Next | Vol- | Vol+ | Play |
+ * |      |      |      |      |      |             |      |      |      |      |      |
  * `-----------------------------------------------------------------------------------'
  */
 [_LOWER] = {
-  {KC_GRV, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR,    KC_ASTR,    KC_LPRN, KC_RPRN, KC_DEL},
-  {KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_UNDS,   KC_PLUS,    KC_LCBR, KC_RCBR, KC_TRNS},
-  {_______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_QUOT,   S(KC_NUBS), KC_HOME, KC_END,  KC_PIPE},
-  {_______, _______, _______, _______, _______, _______, _______, _______,   KC_MNXT,    KC_VOLD, KC_VOLU, KC_MPLY}
+  {KC_GRV,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL},
+  {KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_TRNS},
+  {_______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, KC_QUOT, _______, _______, _______},
+  {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______}
 },
 
 /* Raise
@@ -92,7 +92,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |ISO # |ISO / |Pg Up |Pg Dn |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |             |      | Next | Vol- | Vol+ | Play |
+ * |      |      |      |      |      |             |      |      |      |      |      |
  * `-----------------------------------------------------------------------------------'
  */
 [_RAISE] = {
